df03a9589a1cfc5cf224f03a2e72a1ae3a1c438c
joelkim <kim.dohhyoung@gmail.com>
2026-01-19 16:18:02 +0900

    save

--- preview.sh
+++ preview.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-code --openExternal "http://127.0.0.1:8080/"
-quarto preview "index.ipynb" --no-browser --host 0.0.0.0 --port 8080
+quarto preview "index.ipynb" --host 0.0.0.0 --port 8080
